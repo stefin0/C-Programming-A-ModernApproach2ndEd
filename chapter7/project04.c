f685ae1fd2fdcc92ce988810f376b79ff1625e36
stefin <stefinracho@gmail.com>
2024-10-03 19:46:43 -0700

    complete project 4

--- chapter7/project04.c
+++ chapter7/project04.c
@@ -8,10 +8,33 @@ int main(void) {
   c = getchar();
   while (c != '\n') {
     c = toupper(c);
-    if (c == 'A' || c == 'B' || c == 'C') {
-      putchar('2');
-    } else {
-      putchar(c);
+    switch (c) {
+      case 'A': case 'B': case 'C': 
+        putchar('2');
+        break;
+      case 'D': case 'E': case 'F':
+        putchar('3');
+        break;
+      case 'G': case 'H': case 'I':
+        putchar('4');
+        break;
+      case 'J': case 'K': case 'L':
+        putchar('5');
+        break;
+      case 'M': case 'N': case 'O':
+        putchar('6');
+        break;
+      case 'P': case 'R': case 'S':
+        putchar('7');
+        break;
+      case 'T': case 'U': case 'V':
+        putchar('8');
+        break;
+      case 'W': case 'X': case 'Y':
+        putchar('9');
+        break;
+      default:
+        putchar(c);
     }
     c = getchar();
   }
